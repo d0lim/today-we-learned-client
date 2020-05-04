63a476537b46e3cf03b9181ff77a3e56e1266814
DoLim <limdongyoung0@gmail.com>
2020-05-04 10:51:35 +0000

    Trying ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import "./App.css";
-import { Layout, Menu, Breadcrumb } from "antd";
-import {
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined,
-} from "@ant-design/icons";
+import { Layout, Menu, Breadcrumb, DatePicker } from "antd";
 
-const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
 const App = () => (
@@ -15,11 +9,7 @@ const App = () => (
     <Layout>
       <Header className="header">
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
+        <DatePicker />
       </Header>
       <Layout>
         <Sider width={200} className="site-layout-background">
@@ -29,28 +19,12 @@ const App = () => (
             defaultOpenKeys={["sub1"]}
             style={{ height: "100%", borderRight: 0 }}
           >
-            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-              <Menu.Item key="1">option1</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-              <Menu.Item key="5">option5</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub3"
-              icon={<NotificationOutlined />}
-              title="subnav 3"
-            >
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
-            </SubMenu>
+            {/* <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1"> */}
+            <Menu.Item key="1">Study</Menu.Item>
+            <Menu.Item key="2">Exercise</Menu.Item>
+            <Menu.Item key="3">Cafe</Menu.Item>
+            {/* <Menu.Item key="4">option4</Menu.Item> */}
+            {/* </SubMenu> */}
           </Menu>
         </Sider>
         <Layout style={{ padding: "0 24px 24px" }}>
@@ -67,7 +41,7 @@ const App = () => (
               minHeight: 280,
             }}
           >
-            Content
+            Content Which will be Changed like SPA
           </Content>
         </Layout>
       </Layout>
