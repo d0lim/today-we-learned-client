7dc05714b5316f26b613b2f669d51eb89abae6bb
DoLim <limdongyoung0@gmail.com>
2020-05-05 05:47:05 +0000

    Chakra-ui Header example

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,24 @@
 import React from "react";
 import "./App.css";
-import { Layout, Menu, Breadcrumb, DatePicker } from "antd";
+import Header from "./Header";
+import { theme, ThemeProvider, CSSReset } from "@chakra-ui/core";
 
-const { Header, Content, Sider } = Layout;
+// const breakpoints = ["360px", "768px", "1024px", "1440px"];
+// breakpoints.sm = breakpoints[0];
+// breakpoints.md = breakpoints[1];
+// breakpoints.lg = breakpoints[2];
+// breakpoints.xl = breakpoints[3];
+
+// const newTheme = {
+//   ...theme,
+//   breakpoints,
+// };
 
 const App = () => (
-  <div className="App">
-    <Layout>
-      <Header className="header">
-        <div className="logo" />
-        <DatePicker />
-      </Header>
-      <Layout>
-        <Sider width={200} className="site-layout-background">
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            defaultOpenKeys={["sub1"]}
-            style={{ height: "100%", borderRight: 0 }}
-          >
-            {/* <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1"> */}
-            <Menu.Item key="1">Study</Menu.Item>
-            <Menu.Item key="2">Exercise</Menu.Item>
-            <Menu.Item key="3">Cafe</Menu.Item>
-            {/* <Menu.Item key="4">option4</Menu.Item> */}
-            {/* </SubMenu> */}
-          </Menu>
-        </Sider>
-        <Layout style={{ padding: "0 24px 24px" }}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-            }}
-          >
-            Content Which will be Changed like SPA
-          </Content>
-        </Layout>
-      </Layout>
-    </Layout>
-  </div>
+  <ThemeProvider theme={theme}>
+    <CSSReset />
+    <Header />
+  </ThemeProvider>
 );
 
 export default App;
