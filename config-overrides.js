3952a02217101801368f56d419a73fb0aa453ec4
DoLim <limdongyoung0@gmail.com>
2020-05-05 05:46:56 +0000

    Reset webpack config overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,1 @@
-const { override, addWebpackPlugin  } = require('customize-cra');
-// const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
-const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
-
-module.exports = override(
-    addWebpackPlugin(new AntdDayjsWebpackPlugin())
-);
+const { override, addWebpackPlugin } = require("customize-cra");
